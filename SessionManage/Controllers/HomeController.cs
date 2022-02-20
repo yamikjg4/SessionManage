8e93a6601430dab71a7b6a0f1d6488330c8e4859
yamikjg4 <yamikgandhi@outlook.com>
2022-02-20 15:57:30 +0530

    new

---
+++ SessionManage/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SessionManage.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SessionManage.Controllers
+{
+    public class HomeController : Controller
+    {
+    private readonly IHttpContextAccessor _https;
+    public HomeController(IHttpContextAccessor http)
+        {
+            _https = http;
+        }
+     
+    public IActionResult setsession()
+        {
+            _https.HttpContext.Session.SetString("uname","Yamik");
+            return View();
+        }
+        public IActionResult getsession()
+        {
+            return View();
+        }
+    }
+}
